69ca0908c62b8cccabcf1d087dfb75ad78781b51
Aguinaldo <guiga_oli@hotmail.com>
2024-05-22 14:24:51 -0300

    implementing nokia and iphone classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone1 = new Iphone("998546647","pro","seila",32);
+
+iphone1.InstalarAplicativo("Telegram");
+
+
+Nokia nokia1 = new Nokia("2312312312", "não muito Pro", "não sei", 55);
+
+nokia1.InstalarAplicativo("Cod Mobile");
